e235858f20b8498e285ede07b5274760c73cfc30
Jonas Reycian B. Saraosos <jsaraosos@fullscale.io>
2022-10-05 22:32:32 +0800

    Created functional api

---
+++ src/Controllers/OperationsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationsController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public OperationsController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpOptions("reloadconfig")]
+        public IActionResult ReloadConfig()
+        {
+            try
+            {
+                var root = (IConfigurationRoot)_config;
+                root.Reload();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
